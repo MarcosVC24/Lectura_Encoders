7501e1b93f3f205986a6a807aab534c35d9e2f8f
Marcos Valenzuela <A01253804@tec.mx>
2024-06-04 14:55:27 -0700

    Versión con lectura de 12 bits, ya se provee el ángulo de rotación

--- main/lectura_encoders.c
+++ main/lectura_encoders.c
@@ -18,27 +18,30 @@
 
 #define I2C_SLAVE_ADDR_LENGHT       I2C_ADDR_BIT_LEN_7
 #define I2C_SLAVE_FREQ_HZ           400000
-#define I2C_SLAVE_SENSOR_ADDR       0X5E
+#define I2C_SLAVE_SENSOR1_ADDR      0X5E
+#define I2C_SLAVE_SENSOR2_ADDR      0X80
 
 #define I2C_RESET                   0X00
 
 #define RAD_TO_DEG                  57.296
-//#define TLV493D_SENSOR_ADDR         0x5E
+#define CONSTANT_8BITS              1.56
+#define CONSTANT_12BITS             0.098
 
 static const char *TAG = "Prueba";
-uint8_t fast_config [4] = {0x00, 0x06, 0x00, 0x40};
-uint8_t dataBuff [2];
+uint8_t fast_config_dev1 [4] = {0b00000000, 0b00000110, 0b00000000, 0b00100000}; // Configuración del Encoder 1
+uint8_t fast_config_dev2 [4] = {0b00000000, 0b00100110, 0b00000000, 0b00100000};
+uint8_t dataBuff [5];
 
-static int8_t conversion_table [8] = {-128, 64, 32, 16, 8, 4, 2, 1};
+static int8_t conversion_table_8bits [8] = {-128, 64, 32, 16, 8, 4, 2, 1}; // Tabla de conversión para 8 bits
+static int16_t conversion_table_12bits [12] = {-2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1}; // Tabla de conversión para 12 bits
 
 void i2c_config(){
-
 }
 
 void app_main(void){
     // i2c_config();
     
-    // Se configura el bus i2c
+    // Se configuran los buses i2c //
     i2c_master_bus_handle_t i2c_bus_handle = NULL;
 
     i2c_master_bus_config_t bus_config = {
@@ -51,61 +54,75 @@ void app_main(void){
     };
     ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &i2c_bus_handle)); // Se setea la configuración
     ESP_LOGI(TAG, "I2C configurado");
+    // Se termina la configuración de buses //
 
-    i2c_master_dev_handle_t i2c_dev_handle = NULL;
+    // SE configuran los dispositivos del bus (los encoders) //
+    i2c_master_dev_handle_t i2c_dev1_handle = NULL;
 
     i2c_device_config_t tlv493d_config = {
         .dev_addr_length = I2C_SLAVE_ADDR_LENGHT,
-        .device_address = I2C_SLAVE_SENSOR_ADDR,
+        .device_address = I2C_SLAVE_SENSOR1_ADDR,
         .scl_speed_hz = I2C_SLAVE_FREQ_HZ,
     };
 
-    i2c_master_dev_handle_t i2c_reset = NULL;
+    i2c_master_dev_handle_t i2c_reset = NULL; // Se genera la dirección 0x00 para el reset de los dispositivos
 
     i2c_device_config_t reset = {
         .dev_addr_length = I2C_SLAVE_ADDR_LENGHT,
         .device_address = I2C_RESET,
         .scl_speed_hz = I2C_SLAVE_FREQ_HZ,
     };
-   
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &tlv493d_config, &i2c_dev_handle));
-    ESP_LOGI(TAG, "Handle configurado");
+    // Se terminan de configurar los encoders //
+
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &tlv493d_config, &i2c_dev1_handle));
+    ESP_LOGI(TAG, "Handle 1 configurado");
+
+    vTaskDelay(pdMS_TO_TICKS(40));
 
     ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &reset, &i2c_reset));
     ESP_LOGI(TAG, "Reset handle configurado");
 
-    vTaskDelay(pdMS_TO_TICKS(40));
-
     ESP_ERROR_CHECK(i2c_master_receive(i2c_reset, dataBuff, 1, -1));
     ESP_LOGI(TAG, "Encoder Reiniciado");
 
-    ESP_ERROR_CHECK(i2c_master_probe(i2c_bus_handle, I2C_SLAVE_SENSOR_ADDR, 10));
-    ESP_LOGI(TAG, "Prueba realizada");
-
-    ESP_ERROR_CHECK(i2c_master_transmit(i2c_dev_handle, fast_config, 4, -1));
+    ESP_ERROR_CHECK(i2c_master_transmit(i2c_dev1_handle, fast_config_dev1, 4, -1));
     ESP_LOGI(TAG, "Al parecer se ha configurado el sensor de manera exitosa");
 
+    ESP_ERROR_CHECK(i2c_master_probe(i2c_bus_handle, I2C_SLAVE_SENSOR1_ADDR, 10));
+
     while(1){
-        ESP_ERROR_CHECK(i2c_master_receive(i2c_dev_handle, dataBuff, 2, 10));
+        ESP_ERROR_CHECK(i2c_master_receive(i2c_dev1_handle, dataBuff, 5, 10));
         float realValue;
 
-        for (uint8_t i = 0; i < 8; i++){
-            if (dataBuff[0] & (1 << (7 - i))){
-                realValue += conversion_table[i];
-            }
-        }
-        float valorCampo = realValue * 1.56;
-        realValue = 0.0;
-        for (uint8_t i = 0; i < 8; i++){
-            if (dataBuff[1] & (1 << (7 - i))){
-                realValue += conversion_table[i];
+        uint16_t data_12Bits = dataBuff[0];
+        data_12Bits = data_12Bits << 4;
+        data_12Bits = data_12Bits | (dataBuff[4] >> 4);
+
+        for (uint8_t i = 0; i < 12; i++){
+            if (data_12Bits & (1 << (11 - i))){
+                realValue += conversion_table_12bits[i];
             }
         }
 
-        float valorCampo2 = realValue * 1.56;
+        float valorCampo = realValue * CONSTANT_12BITS;
+        realValue = 0.0;
+        data_12Bits = 0;
+
+        data_12Bits = dataBuff[1];
+        data_12Bits = data_12Bits << 4;
+        data_12Bits = data_12Bits | (dataBuff[4] & 0b00001111);
+
+        for (uint8_t i = 0; i < 12; i++){
+            if (data_12Bits & (1 << (11 - i))){
+                realValue += conversion_table_12bits[i];
+            }
+        }
+        float valorCampo2 = realValue * CONSTANT_12BITS;
+
         float rotacion_rad = atan2(valorCampo, valorCampo2);
         float rotacion_deg = rotacion_rad * RAD_TO_DEG;
         rotacion_deg -= 148.17; // Offset para cero
+
         if (rotacion_deg < 0){
             rotacion_deg = 180 + (180 - fabs(rotacion_deg));
         }
